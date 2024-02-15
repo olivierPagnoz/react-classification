940809cfa8ec25521c41652d412861d5b94896f5
olivierPagnoz <olivier.pagnoz@gmail.com>
2024-02-15 11:24:09 +0100

    fichier supp

--- src/pages/Presentationfamille.js
+++ src/pages/Presentationfamille.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 const Presentationfamille = (props) => {
 
@@ -22,29 +21,4 @@ const Presentationfamille = (props) => {
     );
 };
 
-=======
-
-const Presentationfamille = (props) => {
-
-    return (
-        <div className='presentationfamille' >
-            <div className="titrepresfamille" style={{backgroundColor:props.recup.couleur}}>
-             {props.recup.affiche} {props.nombre} 
-            </div>
-
-            <div className="listeatomecolonne" style={{overflowX : props.recup.idserie === 'transition' ? 'scroll' : 'hidden'}}>
-           {props.liste.map((atome) =>
-                         (
-   
-                    <div key={atome.id} >
-                        <li>{atome.nom_francais}</li>
-                    </div>
-                ))}   
-             
-             </div>
-        </div>
-    );
-};
-
->>>>>>> 3584c84def0286cb40d5a56706833511535dbc8f
 export default Presentationfamille;
